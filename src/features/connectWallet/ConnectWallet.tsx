3de63090ce2c216c619324f48cd03fbf30a96373
alexanderpo <alexander.pozhidaev@gmail.com>
2024-10-13 03:31:17 +0700

    hotfix

--- src/features/connectWallet/ConnectWallet.tsx
+++ src/features/connectWallet/ConnectWallet.tsx
@@ -1,38 +1,29 @@
 import { ConnectButton } from "@mysten/dapp-kit";
 import { Text } from "../../common/components/text";
 import { Heading } from "../../common/components/heading";
-import { Divider } from "../../common/components/divider";
 
 export function ConnectWallet() {
   return (
-    <>
-      <div className="mx-auto max-w-3xl">
-        <Heading>
-          Welcome to{" "}
-          <span className="font-light tracking-wide text-yellow-600 dark:text-yellow-500">
-            Keystore
-          </span>
-        </Heading>
-        <Divider className="my-10 mt-6" soft />
-
-        <section className="grid sm:grid-cols-1">
-          <Text>
-            Keystore is a secure and decentralized storage solution built on the
-            SUI blockchain, designed to protect your most sensitive information.
-            Whether it’s private keys, passwords, or credit card details,
-            Keystore ensures that your data remains encrypted and accessible
-            only to you, providing the ultimate peace of mind in safeguarding
-            your digital assets.
-          </Text>
-        </section>
-
-        <Divider className="my-10" />
-
-        <div className="flex gap-4 items-center flex-wrap">
+    <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8 pb-16 pt-20 text-center lg:pb-32 lg:pt-32">
+      <Heading >
+        <div className="font-display text-4xl font-medium tracking-tight sm:text-5xl">
+        Welcome to{" "}
+        <span className="font-light tracking-wide text-yellow-600 dark:text-yellow-500">
+          Keystore
+        </span>
+        </div>
+      </Heading>
+      <Text className="mx-auto mt-10 max-w-2xl text-lg tracking-tight">
+        Keystore is a secure and decentralized storage solution built on the SUI
+        blockchain, designed to protect your most sensitive information. Whether
+        it’s private keys, passwords, or credit card details, Keystore ensures
+        that your data remains encrypted and accessible only to you, providing
+        the ultimate peace of mind in safeguarding your digital assets.
+      </Text>
+      <div className="my-10 flex gap-4 justify-center items-center flex-wrap">
           <ConnectButton className="!bg-slate-800 dark:!bg-slate-600 !text-white !py-2.5 !px-10" />
           <Text>Please connect to wallet.</Text>
         </div>
-      </div>
-    </>
+    </div>
   );
 }
